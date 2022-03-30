263d27e8b7a2636554fe7ab0b5ab953e312a6cda
cesar <cesar@pop-os.localdomain>
2022-03-30 13:34:20 -0500

    Add router for returning customer page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,6 +15,14 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/NewCustomer.vue')
+    },
+    {
+      path: '/returning_customer',
+      name: 'returning_customer',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/ReturningCustomer.vue')
     }
   ]
 })
