77b3b186485338ee05f0e8ff5b2e99ee7a8fb971
Cesar Cortez <cctez36@gmail.com>
2022-04-10 15:26:48 -0500

    Change queries to work with new DB schema

--- backend/db_connection.py
+++ backend/db_connection.py
@@ -3,14 +3,12 @@ import pymssql
 # server = 'localhost\sqlexpress' # for a named instance
 # server = 'myserver,port' # to specify an alternate port
 def db():
-    server = 'localhost' 
-    database = 'local_cis3365' 
-    username = 'sa' 
-    password = 'Password55!' 
+    server = 'cot-cis3365-03.cougarnet.uh.edu:1433' 
+    database = 'InnovaDB' 
+    username = 'admin' 
+    password = '1234' 
 
     conn = pymssql.connect(server, username, password, database)
-    cursor = conn.cursor()
-    cursor.execute('SELECT * FROM Customer_Info')
     return conn
 
 
--- backend/main.py
+++ backend/main.py
@@ -19,72 +19,54 @@ app.add_middleware(
 )
 
 #Initializing our database connection. This should be changed to your local database, or the remote database on the windows remote desktop
-server = 'localhost' 
-database = 'local_cis3365' 
-username = 'sa' 
-password = 'Password55!' 
-
-conn = pymssql.connect(server, username, password, database)
+conn = db_connection.db()
 cursor = conn.cursor()
 dict_cursor = conn.cursor(as_dict=True)
 
 #This gets the maximum id of a table. However, this should not be needed if we properly add autoincrement to our database.
-def get_max_id(table: str, column_name: str, cursor) -> int:
-    '''
-    Autoincrement in SQL Server is funky, so using this in the meantime. 
-    Might change later when autogeneration is set up. MYSQL > SQL SERVER
-    '''
-    print(table, column_name)
-    max_id_query = f'SELECT MAX({column_name}) FROM {table}'
-    #max_id_query = "SELECT * FROM Customer_Info"
-    cursor.execute(max_id_query)
-    index = cursor.fetchone()[0]
-    print(index)
-    if index is None:
-        index = 0
-    
-    return index + 1
 
 #Creating a structure used to store customer data when a request is ran
 class CustomerInfo(BaseModel):
-    customer_name: str
+    first_name: str
+    middle_initial : str
+    last_name: str
     company_name: str
     email: str
     address: str
-    citystate: str
     zip: str
+    city: str
+    state: int
+    country: int
     phone1: str
     phone2: Optional[str]
 
 #Creating a new endpoint called /new_customer, that takes in customer_info, which is a JSON strucutre in format above, as a parameter
 @app.post("/new_customer")
 def new_customer(customer_info: CustomerInfo):
-    print('test')
     #Getting the firstname and lastname. This would be changed in the future as customers can enter their names in different ways.
-    customer_firstname, customer_lastname = customer_info.customer_name.split()
     #Getting the customer city and state. We need to create a state table, and only allow choices from entries within that table.
-    customer_city, customer_state = customer_info.citystate.split(",")
     #Getting all our IDS for the tables we are going to use. If we set up autoincrement, this will not be necessary
-    customer_info_id = get_max_id('Customer_Info', 'Customer_Id', cursor)
-    customer_contact_id = get_max_id('Customer_Contact', 'Contact_ID', cursor)
-    customer_location_id =  get_max_id('Customer_Location', 'Customer_Location_ID', cursor)
+
 
     '''
     For SQL queries, you can either use f strings
     eg. f"INSERT INTO VALUES ({customer_name}, {customer_company})" where customer_name and customer_company are variables,
-    or the way I am doing it by using string interpolation, in which the %d, %s, %s are placeholders, and the placeholders are replaced
+    or the way I am doing it by using string interpolation, in which the %s, %s, %s are placeholders, and the placeholders are replaced
     using variables that are retrieved from cursor.execute'''
     #SQL query to insert into Customer_Info table.
-    customer_info_query = 'INSERT INTO Customer_Info VALUES(%d, %s, %s)'
-    cursor.execute(customer_info_query, (customer_info_id, customer_firstname, customer_lastname))
-
+    customer_info_query = 'INSERT INTO CUSTOMER VALUES(%s, %s, %s)'
+    cursor.execute(customer_info_query, (customer_info.first_name, customer_info.middle_initial, customer_info.last_name))
+    customer_id = cursor.lastrowid
+    print(customer_id)
+    conn.commit()
     #SQL query to insert into customer contact table, which has a relationship to the customer_info table. The null value should be changed in the future.
-    customer_contact_query = 'INSERT INTO Customer_Contact VALUES (%d, %s, %s, null, %s, %d)'
-    cursor.execute(customer_contact_query, (customer_contact_id, customer_info.phone1, customer_info.phone2, customer_info.email, customer_info_id))
-    
+    customer_contact_query = 'INSERT INTO Customer_Contact VALUES (%s, %s, %s, %s)'
+    cursor.execute(customer_contact_query, (customer_id, customer_info.phone1, customer_info.phone2, customer_info.email))
+    conn.commit()
     #SQL query to insert into customer_location table,
-    customer_location_query = 'INSERT INTO Customer_Location VALUES (%d, %s, %s, %s, %d)'
-    cursor.execute(customer_location_query, (customer_location_id, customer_city, customer_state, customer_info.zip, customer_info_id))
+    customer_location_query = 'INSERT INTO Customer_Address VALUES (%s, %s, %s, %s, %s, %s)'
+    print(customer_id, customer_info.address, customer_info.city, customer_info.state, customer_info.zip, customer_info.country)
+    cursor.execute(customer_location_query, (customer_id, customer_info.address, customer_info.city, customer_info.state, customer_info.zip, customer_info.country))
 
     #After an insert, delete, alter, modify, ec SQL command is done, a conn.commit() is needed to save changes within DB
     conn.commit()
@@ -92,7 +74,7 @@ def new_customer(customer_info: CustomerInfo):
     return {
         "status": "SUCCESS",
         "data": customer_info,
-        "customer_id": customer_info_id
+        "customer_id": customer_id
     }
 
 @app.get("/get_customer")
